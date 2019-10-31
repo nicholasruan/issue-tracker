504a55d31128a6e81004c37345fba818ac777b87
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-10-31 13:47:10 -0400

    added bg image

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,29 +1,34 @@
 import React, { useState } from 'react';
 import Signup from '../components/Signup';
 import Login from '../components/Login';
+import bgImage from '../assets/landing-img.png'
 
 function Landing(props) {
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignup] = useState(false);
 
   return (
-    <div>
-      <h1>Get your<br/>work done.<br/>Together.</h1>
+    <div className="outer-container">
+      <div className="landing-container">
+        <h1 className="landing-title">Issue Tracker</h1>
+        <h2 className="landing-text">Get your<br/>work done.<br/>Together.</h2>
 
-      <div className="landing-button-container">
-        <button className="landing-button" onClick={() => {
-          setShowLogin(true); 
-          setShowSignup(false); 
-        }}>Login</button>
+        <div className="landing-button-container">
+          <button className="landing-button" onClick={() => {
+            setShowLogin(true); 
+            setShowSignup(false); 
+          }}>Login</button>
 
-        <button className="landing-button" onClick={() => {
-          setShowSignup(true); 
-          setShowLogin(false); 
-          }}>Signup</button>
+          <button className="landing-button" onClick={() => {
+            setShowSignup(true); 
+            setShowLogin(false); 
+            }}>Signup</button>
+        </div>
+        
+        {showLogin ? <Login /> : null}
+        {showSignup ? <Signup /> : null}
       </div>
-      
-      {showLogin ?<Login /> : null}
-      {showSignup ? <Signup /> : null}
+      <img className="landing-bg" src={bgImage} alt="background"></img>
     </div>
   )
 }
