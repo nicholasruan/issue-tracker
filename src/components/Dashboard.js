003bfec43329cc7f43aca1095ec7cc3f4b87aced
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-11-03 10:51:05 -0500

    show list of projects and link to projects

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Project from './Project';
 import ProjectForm from './ProjectForm';
 import Modal from './Modal';
 import useModal from '../hooks/useModal';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Dashboard(props) {
   const {isShowing, toggle} = useModal();
+  const [projectList, setProjects] = useState(['']);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const renderProjects = () => {
+  useEffect(() => {
+    
+    axios.get(`https://issue-base-db.herokuapp.com/api/users/${localStorage.user_id}`, 
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.token
+      }
+    }).then(res => {
+      console.log(res);
+      setProjects(res.data.project_ids);
+      setIsLoading(false);
+    }).catch(err => {
+      console.log(err);
+    })
+  }, []);
 
+
+  if (isLoading) {
+    return (
+      <h1>Loading...</h1>
+    )
   }
 
   return (
@@ -16,7 +40,14 @@ function Dashboard(props) {
       <div>
         <h2>Ongoing Projects</h2>
         <div className="dashboard-projects">
-          {renderProjects}
+            <ul>
+              {projectList.map(item => (
+                <li key={item[0]}>
+                  <Link to={`/app/projects/${item[0]}`}>{item[1]}</Link>
+                </li>
+              ))}
+            </ul>
+
           <button className="modal-button" onClick={toggle}>
             +
           </button>
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,7 @@ import React from 'react';
 import List from './List';
 
 function Project(props) {
+  console.log(window.location.pathname);
   return (
     <div>
       Project
