beaef6474006fa197eac95153b7d3f249a2c3ed6
Nicholas Ruan <nicholas.ruan.nr@gmail.com>
2019-11-03 19:38:06 -0500

    fixed layout of projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import List from './List';
+import axios from 'axios';
 
 function Project(props) {
-  console.log(window.location.pathname);
+  const[id, setId] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [title, setTitle] = useState('');
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    const path = window.location.pathname.split('/');
+    setId(path[3]);
+    axios.get(`https://issue-base-db.herokuapp.com/api/projects/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.token
+      }
+    }).then(res => {
+      console.log(res.data);
+      setTitle(res.data.title);
+      setMembers(res.data.members);
+      setIsLoading(false);
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [id]);
+
   return (
-    <div>
-      Project
+    <div className="project-container">
+      {isLoading ? (<h3>Loading...</h3>) : (
+        <div className="project-header">
+          <h2 className="project-title">{title}</h2>
+          <button className="add-list-button">+ add new list</button>
+          {members.map((members, key) => (
+            <p key={key}>{members}</p>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
