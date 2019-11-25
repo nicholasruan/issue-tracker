9aee20f5a64b5a874d18ed50d0ec557211edc7e0
chrisyuaners <chrisyuaners@gmail.com>
2019-11-24 22:47:27 -0500

    render card menu

--- src/components/CardMenu.js
+++ src/components/CardMenu.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function CardMenu(props) {
   return (
-    <div>
+    <div className="card-menu-container">
+      <div className="card-menu-item">
+        <FontAwesomeIcon icon={faEdit} /> Edit
+      </div>
+      <div className="card-menu-item">
+        <FontAwesomeIcon icon={faTrashAlt} /> Delete
+      </div>
     </div>
   )
 }
